8ce4c2d16d94e124ab556b4b152ac3698a9742c3
Hilario J. Montolliu <hmontoliu@gmail.com>
2015-05-25 18:55:36 +0200

    template para copias del propio perfil

---
+++ documentosycorreo.bat
@@ -0,0 +1,16 @@
+@echo off
+call lib_copias.bat
+
+:: Copia de perfil y correo típica para el "usuario" de W7 que ejecuta el script:
+set logfile=%log_dir%\documentos_y_correo_%log_suf%
+set destino="D:\copias_seguridad\%USERNAME%"
+%rbcpy% "%USERPROFILE%\Documents" "%destino%\Documents" %opts% /LOG:%logfile%
+%rbcpy% "%USERPROFILE%\Pictures" "%destino%\Pictures" %opts% /LOG+:%logfile%
+%rbcpy% "%USERPROFILE%\Music" "%destino%\Music" %opts% /LOG+:%logfile%
+%rbcpy% "%USERPROFILE%\Videos" "%destino%\Videos" %opts% /LOG+:%logfile%
+%rbcpy% "%USERPROFILE%\AppData\Local\Microsoft\Outlook" "%destino%\Outlook_AppData" %opts% /LOG+:%logfile%
+
+
+
+
+
---
+++ lib_copias.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enableextensions
+
+:: editar preferentemente con notepad++ 
+
+:: Opciones recomenadadas para robocopy 
+set opts=/NP /MIR /R:1 /W:1 /XJ /FFT /TEE
+
+:: /NP        - No mostrar el progreso (opcional)  
+:: /MIR       - Copia en espejo. Si no es necesario utilizar /E 
+:: /R:1 /W:1  - Tiempos de espera y reintento 
+:: /XJ        - Excludes junction points (W7 y sup) 
+:: /FFT       - two-second precision (útil al copiar a Samba-linux) 
+:: /TEE       - Mostar en pantalla cuando se usa /LOG  
+:: /MT        - Multitask (no usar en entornos sin recursos) 
+
+:: W7 o superior: 
+set rbcpy=robocopy
+
+:: XP2003 o inferior necesitan windows resource kit 
+:: set rbcpy="C:\Archivos de programa\Windows Resource Kits\Tools\robocopy" 
+
+set log_dir="C:\_backups\"  :: dir para los logs
+set log_suf="%DATE:/=%.log" :: sufijo con la fecha
+
+md %log_dir%
+
+
+
+
+
