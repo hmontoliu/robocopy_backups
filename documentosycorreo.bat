fd647d94af33187731fd082dfc3d67146167dc30
Hilario J. Montolliu <hmontoliu@gmail.com>
2015-05-25 19:39:45 +0200

    Add Desktop folder to user's backup

--- documentosycorreo.bat
+++ documentosycorreo.bat
@@ -4,6 +4,7 @@ call lib_copias.bat
 :: Copia de perfil y correo típica para el usuario de W7 que ejecuta el script:
 set logfile=%log_dir%\documentos_y_correo_%log_suf%
 set destino=D:\copias_seguridad\%USERNAME%
+%rbcpy% %USERPROFILE%\Desktop %destino%\Desktop %opts% /LOG:%logfile%
 %rbcpy% %USERPROFILE%\Documents %destino%\Documents %opts% /LOG:%logfile%
 %rbcpy% %USERPROFILE%\Pictures %destino%\Pictures %opts% /LOG+:%logfile%
 %rbcpy% %USERPROFILE%\Music %destino%\Music %opts% /LOG+:%logfile%
