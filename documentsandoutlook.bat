d7bd40334375f34beddfd62235d5eebe624ef8bf
Hilario J. Montolliu <hmontoliu@gmail.com>
2015-06-11 08:19:00 +0200

    forloop alternative for a terse syntax

--- documentsandoutlook.bat
+++ documentsandoutlook.bat
@@ -5,13 +5,16 @@ call lib_copias.bat
 set logfile=%log_dir%\documentos_y_correo_%log_suf%
 set destino=D:\copias_seguridad\%USERNAME%
 %rbcpy% %USERPROFILE%\Desktop %destino%\Desktop %opts% /LOG:%logfile%
-%rbcpy% %USERPROFILE%\Documents %destino%\Documents %opts% /LOG:%logfile%
+%rbcpy% %USERPROFILE%\Documents %destino%\Documents %opts% /LOG+:%logfile%
 %rbcpy% %USERPROFILE%\Pictures %destino%\Pictures %opts% /LOG+:%logfile%
 %rbcpy% %USERPROFILE%\Music %destino%\Music %opts% /LOG+:%logfile%
 %rbcpy% %USERPROFILE%\Videos %destino%\Videos %opts% /LOG+:%logfile%
 %rbcpy% %USERPROFILE%\AppData\Local\Microsoft\Outlook %destino%\Outlook_AppData %opts% /LOG+:%logfile%
 
-
+:: alternativa
+::  for %%D in (Desktop,Documents,Pictures,Music,Videos) do (
+::    %rbcpy% %VSSUSERPROFILE%\%D %destino%\%%D %opts% /LOG+:%logfile%
+::  )
 
 
 
