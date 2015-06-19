a5fe103a65416474a15715e346dd0b3c156e3d7e
hmontoliu <hmontoliu@gmail.com>
2015-06-19 13:39:21 +0200

    Update documentsandoutlook.cmd

--- documentsandoutlook.cmd
+++ documentsandoutlook.cmd
@@ -6,7 +6,7 @@ set destino=D:\copias_seguridad\%USERNAME%
 
 :: Copia de perfil y correo típica para el usuario de W7 que ejecuta el script:
 for %%D in (Desktop,Documents,Pictures,Music,Videos) do (
-  %rbcpy% %VSSUSERPROFILE%\%%D %destino%\%%D %opts% /LOG+:%logfile%
+  %rbcpy% %USERPROFILE%\%%D %destino%\%%D %opts% /LOG+:%logfile%
 )
 
 %rbcpy% %USERPROFILE%\AppData\Local\Microsoft\Outlook %destino%\Outlook_AppData %opts% /LOG+:%logfile%
