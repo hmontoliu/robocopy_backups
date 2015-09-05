7dfc8c4dcffd5d3ddacaa6778e6d141323d242c2
Hilario J. Montolliu <hmontoliu@gmail.com>
2015-09-05 12:27:09 +0200

    refact. simplificar

--- documentsandoutlook.ps1
+++ documentsandoutlook.ps1
@@ -3,27 +3,27 @@
 
 cd $RUNDIR
 
+function rbackup($src, $dest) {
+	<#
+	Llama a: robocopy "origen" "destino" opciones /LOG+:logfile
+	#>
+	$cmd = '& {0} "{1}" "{2}" {3} /LOG+:{4}' -f $rbcpy, $src, $dest, $opts, $logfile
+	Invoke-Expression $cmd
+	}
+
 $logfile="$log_dir\documentos_y_correo_$log_suf"
 $destino="C:\copias_seguridad\${env:USERNAME}"
 
 # Copia de perfil y correo típica para el usuario de W7 que ejecuta el script:
 ForEach ($dir in "Desktop", "Documents", "Pictures", "Music")
 {
-	$src, $dest = "${env:USERPROFILE}/${dir}", "${destino}/${dir}"
-	$cmd = '& {0} "{1}" "{2}" {3} /LOG+:{4}' -f $rbcpy, $src, $dest, $opts, $logfile 
-	Invoke-Expression $cmd
+	rbackup "${env:USERPROFILE}/${dir}" "${destino}/${dir}"
 }
 
 # otros archivos de outlook
-$src = "${env:USERPROFILE}\AppData\Local\Microsoft\Outlook" 
-$dest = "${destino}\Outlook_Appdata"
-$cmd = '& {0} "{1}" "{2}" {3} /LOG+:{4}' -f $rbcpy, $src, $dest, $opts, $logfile
-Invoke-Expression $cmd
+rbackup "${env:USERPROFILE}\AppData\Local\Microsoft\Outlook" "${destino}\Outlook_Appdata"
 
 # siempre respaldar el propio sistema de backup
-#%rbcpy% %RUNDIR% %destino%\_backups %opts% /LOG+:%logfile%
-$dest = "$destino\_backups"
-$cmd = '& {0} "{1}" "{2}" {3} /LOG+:{4}' -f $rbcpy, $rundir, $dest, $opts, $logfile
-Invoke-Expression $cmd
+rbackup "${rundir}" "${destino}\_backups"
 
 # & shutdown -t 0 -f -s
